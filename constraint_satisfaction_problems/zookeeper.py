331e724b6c9f9a6687eb6c315261a2a450126680
iobruno <bruno.ariev@gmail.com>
2020-10-18 05:28:28 -0300

    implementing MustBeTogetherWith constraint

--- constraint_satisfaction_problems/zookeeper.py
+++ constraint_satisfaction_problems/zookeeper.py
@@ -23,14 +23,24 @@ class ZookeeperConstraint:
 
             return assignment[self.animal1] != assignment[self.animal2]
 
-    class MustBeTogether(Constraint[str, str]):
+    class MustBeTogetherWith(Constraint[str, str]):
         def __init__(self, animal, neighbor_animal):
             super().__init__([animal, neighbor_animal])
             self.animal1: str = animal
             self.animal2: str = neighbor_animal
 
         def is_satisfied_with(self, assignment: Dict[str, str]) -> bool:
-            pass
+            """
+            Assignment carries the whole configuration of which animals are assigned to which cage
+            Meaning: if animal2 IS NOT in it, the assignment is free for animal1 to use, and vice-versa
+
+            Otherwise, if both are in it, the constraint is satisfied:
+            AS LONG AS animal1 and animal2 are BOTH in the SAME cage
+            """
+            if self.animal1 not in assignment or self.animal2 not in assignment:
+                return True
+
+            return assignment[self.animal1] == assignment[self.animal2]
 
     class MustBeInCageID(Constraint[str, int]):
         def __init__(self, animal: str, cage: int):

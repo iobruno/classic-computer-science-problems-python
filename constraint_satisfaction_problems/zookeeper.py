442c451850566d7125ef3c75b079afe8e48e4d9c
iobruno <bruno.ariev@gmail.com>
2020-10-18 04:50:39 -0300

    implementing is_satisfied for Reject Constraint

--- constraint_satisfaction_problems/zookeeper.py
+++ constraint_satisfaction_problems/zookeeper.py
@@ -10,8 +10,18 @@ class ZookeeperConstraint:
             self.animal1: str = animal
             self.animal2: str = neighbor_animal
 
-        def is_satisfied_with(self, assignment: Dict[str, str]) -> bool:
-            pass
+        def is_satisfied_with(self, assignment: Dict[str, int]) -> bool:
+            """
+            Assignment carries the whole configuration of which animals are assigned to which cage
+            Meaning: if animal2 IS NOT in it, the assignment is free for animal1 to use, and vice-versa
+
+            Otherwise, if both are in it, the constraint is satisfied:
+            AS LONG AS animal1 and animal2 are NOT in the same cage
+            """
+            if self.animal1 not in assignment or self.animal2 not in assignment:
+                return True
+
+            return assignment[self.animal1] != assignment[self.animal2]
 
     class MustBeTogether(Constraint[str, str]):
         def __init__(self, animal, neighbor_animal):

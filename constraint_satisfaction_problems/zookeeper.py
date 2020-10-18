7ca9ab3f2c6db5399782bb0c0fdb6516f3312e11
iobruno <bruno.ariev@gmail.com>
2020-10-18 05:16:38 -0300

    implementing MustNotBeInAdjacentCageWith

--- constraint_satisfaction_problems/zookeeper.py
+++ constraint_satisfaction_problems/zookeeper.py
@@ -45,3 +45,25 @@ class ZookeeperConstraint:
             for this Constraint to be accepted, otherwise Reject
             """
             return assignment[self.animal] == self.cage_number
+
+    class MustNotBeInAdjacentCageWith(Constraint[str, str]):
+        def __init__(self, animal, another_animal):
+            super().__init__([animal, another_animal])
+            self.animal1 = animal
+            self.animal2 = another_animal
+
+        def is_satisfied_with(self, assignment: Dict[str, int]) -> bool:
+            """
+            Assignment carries the whole configuration of which animals are assigned to which cage
+            Meaning: if animal2 IS NOT in it, the assignment is free for animal1 to use, and vice-versa
+
+            Otherwise, if both are in it, the constraint is satisfied:
+            AS LONG AS animal1 and animal2 are NOT in adjacent cages
+            """
+
+            if self.animal1 not in assignment or self.animal2 not in assignment:
+                return True
+
+            animal1_cage = assignment[self.animal1]
+            animal2_cage = assignment[self.animal2]
+            return abs(animal1_cage - animal2_cage) > 1
